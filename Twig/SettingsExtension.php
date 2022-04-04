b79b8d9624b0fa7cd380daa4db3399ee0c245517
Matthieu <support@pixel-developpement.com>
2022-04-04 16:44:59 +0200

    + Ajout des paramètres

---
+++ Twig/SettingsExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pixel\DirectoryBundle\Twig;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Pixel\DirectoryBundle\Entity\Setting;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class SettingsExtension extends AbstractExtension
+{
+    private $entityManager;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('directory_settings', [$this, 'directorySettings']),
+        ];
+    }
+
+    public function directorySettings()
+    {
+        return $this->entityManager->getRepository(Setting::class)->findOneBy([]);
+    }
+}
